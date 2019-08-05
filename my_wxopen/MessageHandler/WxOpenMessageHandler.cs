14cb99c01c364bbffba5522fde486debe453f36f
hello147 <772637557Q@qq.com>
2019-08-05 21:18:41 +0800

    xd

--- my_wxopen/MessageHandler/WxOpenMessageHandler.cs
+++ my_wxopen/MessageHandler/WxOpenMessageHandler.cs
@@ -66,7 +66,7 @@ namespace my_wxopen.MessageHandler
 
 
         /// <summary>
-        /// 处理文字请求
+        /// 处理文字请求git pull origin master
         /// </summary>
         /// <returns></returns>
         public override IResponseMessageBase OnTextRequest(RequestMessageText requestMessage)
